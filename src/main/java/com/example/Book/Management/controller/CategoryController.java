3fab60263053542917fa64e5260686ab43746f38
Riddhiman Debnath <riddimandebnath4260@gmail.com>
2022-06-06 00:06:24 +0600

    Desing book management category back-end

---
+++ src/main/java/com/example/Book/Management/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package com.example.Book.Management.controller;
+
+import com.example.Book.Management.model.Category;
+import com.example.Book.Management.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/catgory")
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @PostMapping("/create")
+    public String createCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
+        return "success";
+    }
+
+    @GetMapping("/list")
+    public List<Category> listCategory() {
+        return categoryService.listCategory();
+    }
+}
---
+++ src/main/java/com/example/Book/Management/service/CategoryService.java
@@ -0,0 +1,24 @@
+package com.example.Book.Management.service;
+
+import com.example.Book.Management.model.Category;
+import com.example.Book.Management.repository.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    CategoryRepo categoryRepo;
+
+    public void createCategory(Category category) {
+        categoryRepo.save(category);
+    }
+
+    public List<Category> listCategory() {
+        return categoryRepo.findAll();
+    }
+
+}
