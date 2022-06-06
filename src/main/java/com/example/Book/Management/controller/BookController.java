5e189ea2bcc2164d65f702a9892e712b5850b219
Riddhiman Debnath <riddimandebnath4260@gmail.com>
2022-06-06 23:37:05 +0600

    Design book api

---
+++ src/main/java/com/example/Book/Management/controller/BookController.java
@@ -0,0 +1,50 @@
+package com.example.Book.Management.controller;
+
+import com.example.Book.Management.common.ApiResponse;
+import com.example.Book.Management.dto.BookDto;
+import com.example.Book.Management.model.Book;
+import com.example.Book.Management.model.Category;
+import com.example.Book.Management.repository.BookRepo;
+import com.example.Book.Management.repository.CategoryRepo;
+import com.example.Book.Management.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+    @Autowired
+    BookService bookService;
+    @Autowired
+    CategoryRepo categoryRepo;
+    @Autowired
+    BookRepo bookRepo;
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> createBook(@RequestBody BookDto bookDto){
+        Optional<Category> optionalCategory = categoryRepo.findById(bookDto.getCategoryId());
+        if (!optionalCategory.isPresent()){
+            return new ResponseEntity<>(new ApiResponse(false,"Category Does Not Exist"), HttpStatus.BAD_REQUEST);
+        }
+        bookService.createBook(bookDto,optionalCategory.get());
+        return new ResponseEntity<>(new ApiResponse(true,"Book Has Been Added"), HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBook(){
+        List<BookDto> books = bookService.getAllBooks();
+        return new ResponseEntity<>(books,HttpStatus.OK);
+    }
+    @PostMapping("/update{bookId}")
+    public ResponseEntity<ApiResponse> updateBook(@PathVariable("bookId") Integer bookId, @RequestBody BookDto bookDto) throws Exception {
+        Optional<Book> optionalBook = bookRepo.findById(bookId);
+        if (!optionalBook.isPresent()){
+            return new ResponseEntity<>(new ApiResponse(false,"Book Does Not Exist"), HttpStatus.BAD_REQUEST);
+        }
+        bookService.updateBook(bookDto,bookId);
+        return new ResponseEntity<>(new ApiResponse(true,"Book Has Been Updated"), HttpStatus.OK);
+    }
+}
